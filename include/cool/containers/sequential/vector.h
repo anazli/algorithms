f2ea40ac333019724fd5a2008e129da952b63c62
Andreas Nazlidis <andreas221b@gmail.com>
2025-11-15 13:07:01 +0100

    add size and index operator

--- include/cool/containers/sequential/vector.h
+++ include/cool/containers/sequential/vector.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include <memory>
+
 namespace cool {
+
 template <class T>
 class vector {
  public:
-  void temp() {}
+  using iterator = std::unique_ptr<T*>;
+  using const iterator = const std::unique_ptr<T*>;
+  using size_type = size_t;
+
+  vector();
+  vector(size_type n, const T& val = T());
+
+  T& operator[](size_type i) { return m_data[i]; }
+  const T& operator[](size_type i) const { return m_data[i]; }
+
+  size_type size() { return m_end - m_data; }
+
+ private:
+  iterator m_data;
+  iterator m_end;
 };
+
 }  // namespace cool
