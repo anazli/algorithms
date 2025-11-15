4be0aec6814a9a6533b430a4b00234a382e25b2a
Andreas Nazlidis <andreas221b@gmail.com>
2025-11-15 17:11:47 +0100

    update class and tests

--- include/cool/containers/sequential/vector.h
+++ include/cool/containers/sequential/vector.h
@@ -1,16 +1,26 @@
 #pragma once
 
+#include <memory>
+#include <utility>
+
 namespace cool {
 
 template <class T>
 class vector {
  public:
   using iterator = T*;
-  using const iterator = const T*;
+  using const_iterator = const T*;
   using size_type = size_t;
 
   vector();
   explicit vector(size_type n, const T& val = T());
+  ~vector();
+
+  vector(const vector& v);
+  vector& operator=(const vector& v);
+
+  vector(const vector&& v);
+  vector& operator=(const vector&& v);
 
   T& operator[](size_type i) { return m_data[i]; }
   const T& operator[](size_type i) const { return m_data[i]; }
@@ -18,14 +28,51 @@ class vector {
   iterator begin() { return m_data; }
   const_iterator begin() const { return m_data; }
 
-  iterator end() { return m_endl; }
-  const_iterator end() const { return m_endl; }
+  iterator end() { return m_end; }
+  const_iterator end() const { return m_end; }
 
   size_type size() { return m_end - m_data; }
 
  private:
+  void initialize();
+  void destroy();
+
   iterator m_data;
+  iterator m_current_end;
   iterator m_end;
+  std::allocator<T> m_allocator;
 };
+
+template <class T>
+vector<T>::vector() {
+  m_data = m_current_end = m_end = nullptr;
+}
+
+template <class T>
+vector<T>::vector(size_type n, const T& val) {
+  m_data = m_allocator.allocate(n);
+  m_current_end = m_end = m_data + n;
+  std::uninitialized_fill(m_data, m_end, val);
+}
+
+template <class T>
+vector<T>::vector(const vector<T>& v) {
+  m_allocator.allocate(v.end() - v.end());
+  m_current_end = m_end = std::uninitialized_copy(v.begin(), v.end(), m_data);
+}
+
+template <class T>
+vector<T>::~vector() {
+  destroy();
+}
+
+template <class T>
+void vector<T>::destroy() {
+  if (m_data) {
+    std::destroy(begin(), end());
+    m_allocator.deallocate(m_data, m_end - m_data);
+  }
+  m_data = m_current_end = m_end = nullptr;
+}
 
 }  // namespace cool
--- test/test_vector.cpp
+++ test/test_vector.cpp
@@ -1,8 +1,32 @@
 #include <cool/containers/sequential/vector.h>
 #include <gtest/gtest.h>
 
-TEST(vector, Dummy) {
+#include <cmath>
+
+using namespace cool;
+
+class VectorTest : public testing::Test {
+ public:
+};
+
+TEST_F(VectorTest, GivenNoInputSizeWhenVectorIsCreatedThenItIsEmpty) {
   cool::vector<int> v;
-  v.temp();
-  SUCCEED();
+  ASSERT_EQ(0, v.size());
+}
+
+TEST_F(VectorTest, GivenInputSizeWhenVectorIsCreatedThenItsSizeIsCorrect) {
+  auto n = 100;
+  cool::vector<int> v(n);
+  ASSERT_EQ(n, v.size());
+}
+
+TEST_F(VectorTest,
+       GivenInputSizeAndValueWhenVectorIsCreatedThenAllElementsAreInitialized) {
+  auto n = 100;
+  auto val = acos(-1);
+  cool::vector<double> v(n, val);
+  ASSERT_EQ(n, v.size());
+  for (auto& elem : v) {
+    ASSERT_EQ(val, elem);
+  }
 }
