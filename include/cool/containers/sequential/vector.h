70d15cf6f199640933c4ae1b5e591510bc7fa6e6
Andreas Nazlidis <andreas221b@gmail.com>
2025-11-15 13:33:25 +0100

    user raw pointers

--- include/cool/containers/sequential/vector.h
+++ include/cool/containers/sequential/vector.h
@@ -1,21 +1,25 @@
 #pragma once
 
-#include <memory>
-
 namespace cool {
 
 template <class T>
 class vector {
  public:
-  using iterator = std::unique_ptr<T*>;
-  using const iterator = const std::unique_ptr<T*>;
+  using iterator = T*;
+  using const iterator = const T*;
   using size_type = size_t;
 
   vector();
-  vector(size_type n, const T& val = T());
+  explicit vector(size_type n, const T& val = T());
 
   T& operator[](size_type i) { return m_data[i]; }
   const T& operator[](size_type i) const { return m_data[i]; }
+
+  iterator begin() { return m_data; }
+  const_iterator begin() const { return m_data; }
+
+  iterator end() { return m_endl; }
+  const_iterator end() const { return m_endl; }
 
   size_type size() { return m_end - m_data; }
 
