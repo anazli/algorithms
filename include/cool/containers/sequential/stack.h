329c8dc44ae70df7c044fbdf1674602e6b7d9cf0
Andreas Nazlidis <andreas221b@gmail.com>
2026-02-21 23:31:03 +0100

    fix issues

--- include/cool/containers/sequential/stack.h
+++ include/cool/containers/sequential/stack.h
@@ -12,12 +12,24 @@ class stack {
   using const_iterator = const T*;
   using size_type = size_t;
 
-  ~stack();
+  ~stack() { destroy(); }
 
   size_type size() const { return m_current - m_data; }
   bool empty() const { return size() == ptrdiff_t(0); }
 
+  void push(const T& elem) {
+    if (m_current == m_end) {
+      resize();
+    }
+    std::construct_at(m_current++, elem);
+  }
+
+  T top() const { return *(m_current - 1); }
+
  private:
+  void resize();
+  void destroy();
+
   iterator m_data = nullptr;
   iterator m_end = nullptr;
   iterator m_current = nullptr;
@@ -25,12 +37,23 @@ class stack {
 };
 
 template <class T>
-stack<T>::~stack() {
+void stack<T>::destroy() {
   if (m_data) {
-    std::destroy(m_data, m_end);
-    m_allocator.deallocate(m_data, m_end - m_data);
+    std::destroy(m_data, m_current);
+    m_allocator.deallocate(m_data, m_current - m_data);
   }
   m_data = m_end = m_current = nullptr;
+}
+
+template <class T>
+void stack<T>::resize() {
+  size_type s = std::max(2 * (m_end - m_data), ptrdiff_t(1));
+  iterator data = m_allocator.allocate(s);
+  iterator current_end = std::uninitialized_copy(m_data, m_current, data);
+  destroy();
+  m_data = data;
+  m_current = current_end;
+  m_end = m_data + s;
 }
 
 }  // namespace cool
--- test/stack_test.cpp
+++ test/stack_test.cpp
@@ -2,17 +2,26 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-using testing::Eq;
+#include <string>
 
+using testing::Eq;
 using namespace cool;
 
 class StackTest : public testing::Test {
  public:
+  stack<std::string> st;
+  std::string test = std::string("test");
 };
 
 TEST_F(StackTest, GivenTypeWhenItsCreatedThenItsEmpty) {
-  stack<int> st;
-
   EXPECT_THAT(st.size(), Eq(0));
   ASSERT_TRUE(st.empty());
+}
+
+TEST_F(StackTest, GivenEmptyStackWhenPushElemThenItsCorrectlyAdded) {
+  st.push(test);
+
+  EXPECT_THAT(st.top(), Eq(test));
+  EXPECT_THAT(st.size(), Eq(1));
+  ASSERT_FALSE(st.empty());
 }
