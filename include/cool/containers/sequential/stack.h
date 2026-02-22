e50307ed200f66b9cf61d6208b70c307a4ed76f8
Andreas Nazlidis <andreas221b@gmail.com>
2026-02-22 09:04:38 +0100

    update for rule of 5

--- include/cool/containers/sequential/stack.h
+++ include/cool/containers/sequential/stack.h
@@ -12,7 +12,13 @@ class stack {
   using const_iterator = const T*;
   using size_type = size_t;
 
+  stack() = default;
   ~stack() { destroy(); }
+  stack(const stack& v) { initialize_copy(m_data, m_end); }
+  stack(stack&& v) noexcept { initialize_move(m_data, m_end); }
+
+  stack& operator=(const stack& v);
+  stack& operator=(stack&& v) noexcept;
 
   size_type size() const { return m_current - m_data; }
   bool empty() const { return size() == ptrdiff_t(0); }
@@ -30,11 +36,32 @@ class stack {
   void resize();
   void destroy();
 
+  void initialize_copy(const_iterator left, const_iterator right);
+  void initialize_move(const_iterator left, const_iterator right);
+
   iterator m_data = nullptr;
   iterator m_end = nullptr;
   iterator m_current = nullptr;
   std::allocator<T> m_allocator = std::allocator<T>();
 };
+
+template <class T>
+stack<T>& stack<T>::operator=(const stack<T>& v) {
+  if (&v != this) {
+    destroy();
+    initialize_copy(v.begin(), v.end());
+  }
+  return *this;
+}
+
+template <class T>
+stack<T>& stack<T>::operator=(stack<T>&& v) noexcept {
+  if (&v != this) {
+    destroy();
+    initialize_move(v.begin(), v.end());
+  }
+  return *this;
+}
 
 template <class T>
 void stack<T>::destroy() {
@@ -47,13 +74,32 @@ void stack<T>::destroy() {
 
 template <class T>
 void stack<T>::resize() {
-  size_type s = std::max(2 * (m_end - m_data), ptrdiff_t(1));
-  iterator data = m_allocator.allocate(s);
-  iterator current_end = std::uninitialized_copy(m_data, m_current, data);
-  destroy();
-  m_data = data;
-  m_current = current_end;
-  m_end = m_data + s;
+  size_type old_size = size();
+  size_type new_capacity = old_size == 0 ? 1 : 2 * old_size;
+
+  iterator new_data = m_allocator.allocate(new_capacity);
+  std::uninitialized_move(m_data, m_current, new_data);
+
+  if (m_data) {
+    std::destroy(m_data, m_current);
+    m_allocator.deallocate(m_data, m_end - m_data);
+  }
+
+  m_data = new_data;
+  m_current = m_data + old_size;
+  m_end = m_data + new_capacity;
+}
+
+template <class T>
+void stack<T>::initialize_copy(const_iterator left, const_iterator right) {
+  m_data = m_allocator.allocate(right - left);
+  m_current = m_end = std::uninitialized_copy(left, right, m_data);
+}
+
+template <class T>
+void stack<T>::initialize_move(const_iterator left, const_iterator right) {
+  m_data = m_allocator.allocate(right - left);
+  m_current = m_end = std::uninitialized_move(left, right, m_data);
 }
 
 }  // namespace cool
