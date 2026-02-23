ad5b0eb78aec1e5507acf92ca1e5de6d5cda39b4
Andreas Nazlidis <andreas221b@gmail.com>
2026-02-23 19:55:28 +0100

    add selection and bubble sort

--- include/cool/algorithms/sort.h
+++ include/cool/algorithms/sort.h
@@ -15,4 +15,30 @@ void insertion_sort(cool::vector<T>& v) {
   }
 }
 
+template <typename T>
+void selection_sort(cool::vector<T>& v) {
+  auto n = v.size();
+  for (int i = 0; i < n; ++i) {
+    int indx = i;
+    for (int j = i; j < n; ++j) {
+      if (v[j] < v[indx]) {
+        indx = j;
+      }
+    }
+    std::swap(v[i], v[indx]);
+  }
+}
+
+template <typename T>
+void bubble_sort(cool::vector<T>& v) {
+  int n = v.size();
+  for (int i = n - 1; i >= 0; --i) {
+    for (int j = 0; j < i; ++j) {
+      if (v[j] > v[j + 1]) {
+        std::swap(v[j + 1], v[j]);
+      }
+    }
+  }
+}
+
 }  // namespace cool
--- test/sort_test.cpp
+++ test/sort_test.cpp
@@ -9,12 +9,37 @@ using namespace cool;
 
 class SortTest : public testing::Test {
  public:
+  cool::vector<int> v = {5, 3, 9, 0, 4, 5, 2, 6, 8};
+
+  template <typename T>
+  void print(const cool::vector<T>& v) {
+    for (int i = 0; i < v.size(); ++i) std::cout << v[i] << " ";
+    std::cout << std::endl;
+  }
 };
 
 TEST_F(SortTest, GivenNoneEmptyVectorWhenPassedToInsertionSortThenItsSorted) {
-  cool::vector<int> v = {5, 3, 9, 0, 4, 5, 2, 6, 8};
   cool::insertion_sort(v);
 
+  // print(v);
+  for (int i = 1; i < v.size(); ++i) {
+    ASSERT_THAT(v[i], testing::Ge(v[i - 1]));
+  }
+}
+
+TEST_F(SortTest, GivenNoneEmptyVectorWhenPassedToSelectionSortThenItsSorted) {
+  cool::selection_sort(v);
+
+  // print(v);
+  for (int i = 1; i < v.size(); ++i) {
+    ASSERT_THAT(v[i], testing::Ge(v[i - 1]));
+  }
+}
+
+TEST_F(SortTest, GivenNoneEmptyVectorWhenPassedToBubbleSortThenItsSorted) {
+  cool::bubble_sort(v);
+
+  print(v);
   for (int i = 1; i < v.size(); ++i) {
     ASSERT_THAT(v[i], testing::Ge(v[i - 1]));
   }
